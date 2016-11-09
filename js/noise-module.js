fb231d0c47425f1ed25752bbf7db48b94b3fa1f8
Apostolos <apostolos@projectbeagle.com>
2016-11-09 18:19:53 +0200

    More nodes in module

--- js/noise-module.js
+++ js/noise-module.js
@@ -13,7 +13,37 @@
 
 	$.NoiseModule.defaults		= {
 
-		initialVolume	: 0.7
+		oscillatorFrequency		: 900,
+		oscillatorDetune		: 100,
+
+		biquadFilterFrequency	: 900,
+		biquadFilterDetune		: 400,
+		biquadFilterQ 			: 100,
+		biquadFilterGain		: 25,
+
+		delayTime				: 5.0,
+
+		compressorThreshold 	: -50,
+		compressorKnee			: 40,
+		compressorRatio 		: 12,
+		compressorReduction		: -20,
+		compressorAttack		: 0,
+		compressorRelease		: 0.25,
+
+		gainGain				: 0.7,
+
+		stereoPannerPan			: 0,
+
+		waveShapperCurveAmount	: 400,
+
+		/* none, 2x, 4x */
+		waveShapperOversample	: '4x', 
+
+		periodicWaveRealArray	: new Float32Array( [ 0, 1 ] ),
+		periodicWaveImagArray	: new Float32Array( [ 0, 0 ] ),
+		periodicWaveDisableNorm	: false,
+
+		analyserFftSize			: 2048,
 
 	};
 
@@ -46,13 +76,25 @@
 
 			var radioNode 		= this._createRadioNode();
 
-			var gain 			= this._createGain( 0.7 );
+			var gain 			= this._createGain();
 
+			var biquadFilter 	= this._createBiquadFilter();
+			var delay 			= this._createDelay();
 
-			this._connectNodes ( sineWave, gain );
+			var panner 			= this._createStreoPanner();
+
+			var periodicWave 	= this._connectPeriodicWave ( sineWave );
+
+			this._connectNodes ( sineWave, biquadFilter );
+
+			this._connectNodes ( biquadFilter, delay );
+
+			this._connectNodes ( delay, panner );
+
+			this._connectNodes ( panner, gain );
 			// this._connectNodeToDestination ( gain );
 
-			// console.log ( sineWave );
+			console.log ( sineWave );
 
 		},
 
@@ -158,12 +200,13 @@
 
 		},
 
-		_createOscillator			: function ( type, frequency ) {
+		_createOscillator			: function ( type ) {
 
 			var wave = this.audioContext.createOscillator();
 
-			wave.type = type || "sine";
-			wave.frequency.value = frequency || 900;
+			wave.type = type;
+			wave.frequency.value = this.options.oscillatorFrequency;
+			wave.detune.value = this.options.oscillatorDetune;
 
 			wave.start ( 0 );
 
@@ -171,27 +214,27 @@
 
 		},
 
-		_createSineWave				: function ( frequency ) {
+		_createSineWave				: function ( ) {
 
-			return this._createOscillator ( 'sine', frequency );
-
-		},
-
-		_createSawToothWave			: function ( frequency ) {
-
-			return this._createOscillator ( 'sawtooth', frequency );
+			return this._createOscillator ( 'sine' );
 
 		},
 
-		_createSquareWave			: function ( frequency ) {
+		_createSawToothWave			: function ( ) {
 
-			return this._createOscillator ( 'square', frequency );
+			return this._createOscillator ( 'sawtooth' );
 
 		},
 
-		_createTriangleWave			: function ( frequency ) {
+		_createSquareWave			: function ( ) {
 
-			return this._createOscillator ( 'triangle', frequency );
+			return this._createOscillator ( 'square' );
+
+		},
+
+		_createTriangleWave			: function ( ) {
+
+			return this._createOscillator ( 'triangle' );
 
 		},
 
@@ -220,13 +263,124 @@
 
 		},
 
-		_createGain					: function ( value ) {
+		_createGain					: function ( ) {
 
 			var gain = this.audioContext.createGain ();
 
-			gain.gain.value = value || 0;
+			gain.gain.value = this.options.gainGain;
 
 			return gain;
+
+		},
+
+		_createBiquadFilter			: function ( type ) {
+
+			var node = this.audioContext.createBiquadFilter();
+
+			node.type = type;
+			node.frequency.value = this.options.biquadFilterFrequency;
+			node.detune.value = this.options.biquadFilterDetune;
+			node.Q.value = this.options.biquadFilterQ;
+			node.gain.value = this.options.biquadFilterGain;
+
+			return node;
+
+		},
+
+		_createDelay				: function ( ) {
+
+			var node = this.audioContext.createDelay ();
+
+			node.delayTime.value = this.options.delayTime;
+
+			return node;
+
+		},
+
+		_createDynamicsCompressor	: function ( ) {
+
+			var node = this.audioContext.createDynamicsCompressor ();
+
+			node.threshold.value = this.options.compressorThreshold;
+			node.knee.value = this.options.compressorKnee;
+			node.ratio.value = this.options.compressorRatio;
+			node.reduction.value = this.options.compressorReduction;
+			node.attack.value = this.options.compressorAttack;
+			node.release.value = this.options.compressorRelease;
+
+			return node;
+
+		},
+
+		_createStreoPanner			: function ( ) {
+
+			var node = this.audioContext.createStereoPanner ( );
+
+			node.pan.value = this.options.stereoPannerPan;
+
+			return node;
+
+		},
+
+		_createDistortionCurve		: function ( amount ) {
+
+			var k = typeof amount === 'number' ? amount : 50;
+		    var n_samples = 44100;
+
+		    var curve = new Float32Array(n_samples);
+		    var deg = Math.PI / 180;
+		    var i = 0
+		    var x;
+
+			for ( ; i < n_samples; ++i ) {
+				
+				x = i * 2 / n_samples - 1;
+
+				curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+			}
+
+			return curve;
+
+		},
+
+		_createWaveShaper			: function ( ) {
+
+			var node = this.audioContext.createWaveShaper ( );
+
+			node.curve = this._createDistortionCurve ( this.options.waveShapperCurveAmount );
+			node.oversample = this.options.waveShapperOversample;
+
+			return node;
+
+		},
+
+		_createAnalyser				: function ( ) {
+
+			var analyser = this.audioContext.createAnalyser ( );
+
+			analyser.fftSize = this.options.analyserFftSize;
+
+			var bufferLength = analyser.frequencyBinCount;
+			var dataArray = new Uint8Array ( bufferLength );
+			
+			analyser.getByteTimeDomainData ( dataArray );
+
+			return analyser;
+
+		},
+
+		_connectPeriodicWave		: function ( oscillator ) {
+
+			var wave = this.audioContext.createPeriodicWave(
+				this.options.periodicWaveRealArray, 
+				this.options.periodicWaveImagArray, 
+				{
+					disableNormalization: this.options.periodicWaveDisableNorm
+				});
+
+			oscillator.setPeriodicWave ( wave );
+
+			return wave;
 
 		},
 
